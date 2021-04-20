4fd19392aabc8bb1e94777b3d57ef0cb39469fd7
poporthailand <ismat_rr@hotmail.com>
2021-04-20 22:46:23 +0700

    save page_home

--- clients/src/components/homePage.vue
+++ clients/src/components/homePage.vue
@@ -4,7 +4,7 @@
       <div class="header">
         
       </div>
-      <div class="Body" style=" backgroundImage: url(https://lh3.googleusercontent.com/-gS0V6GutSu7StY9v7Hi5tV8tDQL3cFy91it_QXuL7P_0pO6OLTFDJsNvCrSuBgvadPicA4sY7QQXbcsdsqlpi_vZEcyVSvHQfVdxHsbji_CELXOhkVwPt20bbExkFMFGFVa9IP-ff0PXhWgtyzaoezhfUhDDNUxn7hD1Lyr8B3xCdJ1fCj2YYEuRxpCThXf0JwZvHLCPkKQiRtnZKqJD3_ODJwtuHiEnBq99ov67jE3lqIDAZ2R0YdSK-V-NtnDpGq8K7k9YpL3eCq-OBcEhyspAUKL-0hwtr015oRO4ka1YiuHB1_mdmFnHuPv1HW3Dtbb8PnszWa9Zbul8lkIj6nD_hh4iruHUgMdrDO8wpcbjr8PONL28y6SrzYYgkBZJVQjd_nESAQb7MpQXgrung5cTOcYh1xWL5te7UqUMoZ0Ox2rt7H9yEeVFhJCegFFUqS8cOP7pHd9rUa_3lM4gWIzM1M9U-Jerrxie2WYuq4Vj3wsfLmEi6G42cF92zrcUt026oYY6dyQG9T5IF7RB7UE--9ZdbOH-6akC89SCo8_KxrCTxGs2eb69TotN9xNwbJELTWh7l_sh5QL4sHG_IKz1JSxP06TAkImVP3Jhxr_MNQD49ff8Bka_-6jhqJJC4qtkf51G-B8oI4fiy7uGocxONubmg8MBocw130oQ6MJ8AZHDoc4GzUQ1nbd2WUm8rnQ580bXsYPsUYJdKSlmjwQ=w1920-h715-no?authuser=0) ">
+      <div class="Body" style=" backgroundImage: url(https://1.bp.blogspot.com/-LySYqXBTJ3E/YH7y54hxTvI/AAAAAAAAAAk/aSJx4u79H_4ip60o25mujuP0mY90Zgr2wCLcBGAsYHQ/s1920/3.png) ">
         <div class="bodyleft" >
           <center>
       <img src="../assets/img/main/1.png" style="margin-top: 90px;">
